bb1c24e05df13cf6af50377e62668d9416470208
zakattack9 <zakattack26111@gmail.com>
2016-11-28 08:44:15 -1000

    Looping and Loopin and Loopi and Loop and Loo and Lo and L and

---
+++ Loop-City.js
@@ -0,0 +1,13 @@
+for (var count = 0; count <= 100; count++){
+  console.log("I can count to " + count);
+}
+
+for (var animalCount = 0; animalCount <= 5; animalCount++){
+  console.log("There are " + animalCount + " many animals in Noahs Arc")
+}
+
+var partyAnimals = [hippo, cougar, jaguar, crocodile, lion];
+var i;
+for (i = 0; i <= 4; i++){
+  console.log(partyAnimals[i])
+}
