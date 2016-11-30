8e40f602ba3f0e4badb98fe00a2f7c431b751cde
zakattack9 <zakattack26111@gmail.com>
2016-11-30 09:23:19 -1000

    Looping

--- Loop-City.js
+++ Loop-City.js
@@ -3,11 +3,11 @@ for (var count = 0; count <= 100; count++){
 }
 
 for (var animalCount = 0; animalCount <= 5; animalCount++){
-  console.log("There are " + animalCount + " many animals in Noahs Arc")
+  console.log("There are " + animalCount + " many animals in Noahs Arc");
 }
 
-var partyAnimals = [hippo, cougar, jaguar, crocodile, lion];
-var i;
-for (i = 0; i <= 4; i++){
-  console.log(partyAnimals[i])
+var partyAnimals = ["Hippo", "Cougar", "Bear", "Crocodile", "Lion"];
+var animalPhrases = ["s have nipples", "s eat boogers", "s drink air", "s run miles", "s stay cryin'"];
+for (var i = 0; i <= 4; i++){
+  console.log(partyAnimals[i] + animalPhrases[i] + ".");
 }
